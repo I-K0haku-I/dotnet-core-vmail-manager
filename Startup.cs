b54e6fdfa045dee0d76e8921838c526f4fb7f37b
Andreas S <k0haku@hotmail.com>
2020-08-12 20:19:57 +0200

    add option to define static files root

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;
 using mail_manager.Models;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
@@ -57,7 +59,14 @@ namespace mail_manager
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            Console.WriteLine(env.ContentRootPath);
+            String staticRootPath = Configuration["staticRootPath"];
+            if (staticRootPath == "")
+                staticRootPath = env.ContentRootPath;
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(staticRootPath, "wwwroot"))
+            });
 
             app.UseRouting();
 
